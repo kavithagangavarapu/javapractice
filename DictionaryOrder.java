bce2804d31554bc9695be3065f3ff9de00248767
kavithagangavarapu <117485069+kavithagangavarapu@users.noreply.github.com>
2022-12-20 08:46:45 -0600

    Update DictionaryOrder.java

--- DictionaryOrder.java
+++ DictionaryOrder.java
@@ -5,8 +5,17 @@ import java.util.Arrays;
 public class DictionaryOrder {
 	public static void main(String[] args) {
 		String[] words = { "cat", "dog", "apple", "banana", "Monkey" };
+		for(int i=0; i<5; i++);
+		for(J=i+1; j<4; j++);
+		{ if (words[i].comparetowords[j]>0)
+		 {
+		string temp=words[i];
+		words[i]=words[j];
+		words[j]=temp;
+		 }
+		}
 
-		Arrays.sort(words);
+		
 
 		System.out.println(Arrays.toString(words));
 	}
